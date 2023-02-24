9bd708b43d7a41334930c8815826e445552a196d
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-24 10:53:15 -0800

    second test good compiler fail.

--- BakedGoods.Tests/ModelTests/PastryTests.cs
+++ BakedGoods.Tests/ModelTests/PastryTests.cs
@@ -8,12 +8,22 @@ namespace BakedGoods.Tests
   [TestClass]
   public class PastryTests
   {
-  [TestMethod]
+    [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
       Pastry newDonut = new Pastry(pastryNum);
       Assert.AreEqual(typeof(Pastry), newDonut.GetType());
     }
-
+    [TestMethod]
+    public void GetPastryOrder_SetValueOfPastryOrder_Int()
+    {
+      //Arrange
+      int pastryNum = 1;
+      Pastry newDonut = new Pastry(pastryNum);
+      //Act
+      newDonut.PastryOrder = pastryNum;
+      //Assert 
+      Assert.AreEqual(pastryNum, newDonut.PastryOrder);
+    }
   }
 }
