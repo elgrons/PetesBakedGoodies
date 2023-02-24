becf44cad022dffeaedc69fbde8cc0933ac7f308
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-24 10:50:01 -0800

    first test pass for PastryTest constructor.

--- BakedGoods.Tests/ModelTests/PastryTests.cs
+++ BakedGoods.Tests/ModelTests/PastryTests.cs
@@ -8,6 +8,12 @@ namespace BakedGoods.Tests
   [TestClass]
   public class PastryTests
   {
+  [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newDonut = new Pastry(pastryNum);
+      Assert.AreEqual(typeof(Pastry), newDonut.GetType());
+    }
 
   }
 }
