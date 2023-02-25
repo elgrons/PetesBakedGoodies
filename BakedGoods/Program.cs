80826063a131707ac1a6ec152978aab7e06845b7
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-24 17:03:46 -0800

    update UI.

--- BakedGoods/Program.cs
+++ BakedGoods/Program.cs
@@ -9,11 +9,11 @@ namespace BakedGoods
     static void Main()
     {
       Console.WriteLine("");
-      Console.WriteLine("          Welcome to Pete's Baked Goodies!");
-      Console.WriteLine("   The price of a single loaf of bread is $5 and the price for a single pastry is $2.");
+      Console.WriteLine("*~*~*~*~*Welcome to Pete's Baked Goodies!*~*~*~*~*");
+      Console.WriteLine("The price of a single loaf of bread is $5 and the price for a single pastry is $2.");
       Console.WriteLine("");
-      Console.WriteLine("   ~~But wait! Pete has a special DEAL for you today!~~          ");
-      Console.WriteLine("   ***Bread is Buy 2, get 1 free and Pastry is Buy 3, get 1 free. Discount applied at checkout.*** ");
+      Console.WriteLine("~~But wait! Pete has a special DEAL for you today!~~          ");
+      Console.WriteLine("***Bread is Buy 2, get 1 free and Pastry is Buy 3, get 1 free. Discount applied at checkout.*** ");
       Console.WriteLine("");
       Console.WriteLine("*Please enter a number for how many loaves of bread you would like to order:");
       string breadNumber = Console.ReadLine(); 
@@ -21,15 +21,16 @@ namespace BakedGoods
       string pastryNumber = Console.ReadLine();
       int breadNum = int.Parse(breadNumber);
       Bread bread = new Bread(breadNum);
-      Console.WriteLine($"      Your selected bread amount is: {bread.BreadOrder}.");  
+      Console.WriteLine($"Your selected bread amount is: {bread.BreadOrder}.");  
       int pastryNum = int.Parse(pastryNumber);
       Pastry pastry = new Pastry(pastryNum);
-      Console.WriteLine($"      Your selected pastry amount is: {pastry.PastryOrder}.");  
+      Console.WriteLine($"Your selected pastry amount is: {pastry.PastryOrder}.");  
       int breadTotal = bread.BreadOrderTotal(breadNum);
       int pastryTotal = pastry.PastryOrderTotal(pastryNum);
-      Console.WriteLine("");
-      Console.WriteLine("      Your Bread order total is: $" + breadTotal + " and your Pastry order total is: $" + pastryTotal + ".");
-      Console.WriteLine("");
+      Console.WriteLine(""); 
+      Console.WriteLine("*~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~*");
+      Console.WriteLine("Your Bread order total is: $" + breadTotal + " and your Pastry order total is: $" + pastryTotal + ".");
+      Console.WriteLine("*~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~*");
       Console.WriteLine(""); 
       Console.WriteLine("<3<3<3Thanks for visiting Pete's Baked Goodies! Enjoy your fresh baked goodies and we'll see you tomorrow!<3<3<3");
       Console.WriteLine("");
